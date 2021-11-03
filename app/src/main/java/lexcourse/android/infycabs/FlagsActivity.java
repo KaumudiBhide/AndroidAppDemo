84c39827d1c8810a78d56855fca00e84a6ad4087
KaumudiBhide <kaumudi.bhide.2015@gmail.com>
2021-11-03 21:28:59 +0530

    added flag scren, view, adapter and model classes

--- app/src/main/java/lexcourse/android/infycabs/FlagsActivity.java
+++ app/src/main/java/lexcourse/android/infycabs/FlagsActivity.java
@@ -1,8 +1,16 @@
 package lexcourse.android.infycabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lexcourse.android.infycabs.adapters.FlagAdapater;
+import lexcourse.android.infycabs.data.models.FlagCard;
 
 public class FlagsActivity extends AppCompatActivity {
 
@@ -10,5 +18,30 @@ public class FlagsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flags);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(
+                R.id.recyclerView);
+
+        List<FlagCard> list = new ArrayList<>();
+        list = getData();
+
+        FlagAdapater adapter = new FlagAdapater(list, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(this));
+
     }
+
+    // Flag data for RecyclerView
+    private List<FlagCard> getData()
+    {
+        List<FlagCard> list = new ArrayList<>();
+        for(int i=0; i<Globals.COUNTRIES.length; i++) {
+            list.add(new FlagCard(Globals.FLAGS[i],
+                    Globals.COUNTRIES[i]));
+        }
+
+        return list;
+    }
+
 }
--- app/src/main/java/lexcourse/android/infycabs/Globals.java
+++ app/src/main/java/lexcourse/android/infycabs/Globals.java
@@ -4,15 +4,31 @@ public class Globals {
     public static boolean isNewUser = false;
     public static boolean isUserLoggedIn = false;
 
+    // ride images
     public static final String[] RIDE_NAMES = new String[]{
             "Luxury Ride", "BMW Red", "DDLJ Eurail",
             "BMW White", "Batmobile",
     };
 
+    // ride names
     public static final int[] RIDE_CARS = new int[]{
             R.drawable.ride_luxury_car, R.drawable.ride_bmw_red,
             R.drawable.ride_ddlj_car,
             R.drawable.ride_bmw_white, R.drawable.ride_batmobile,
+    };
+
+    // country names
+    public static final String[] COUNTRIES = new String[]{
+            "America", "Emirates",
+            "Switzerland", "India",
+            "England"
+    };
+
+    // flag images
+    public static final int[] FLAGS = new int[]{
+            R.drawable.ic_flag_us, R.drawable.ic_flag_ae,
+            R.drawable.ic_flag_ch, R.drawable.ic_flag_in,
+            R.drawable.ic_flag_uk
     };
 
     public static final String LOG_TAG = "InfyCabs";
